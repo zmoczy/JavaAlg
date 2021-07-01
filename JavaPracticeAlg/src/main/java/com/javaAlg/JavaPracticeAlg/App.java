ba3c2bc478411964d48351b039e353cb87e9d4fe
Zachary Moczygemba <zachary_moczygemba@hcl.com>
2021-07-01 13:55:33 -0500

    java alg practice

---
+++ JavaPracticeAlg/src/main/java/com/javaAlg/JavaPracticeAlg/App.java
@@ -0,0 +1,13 @@
+package com.javaAlg.JavaPracticeAlg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
